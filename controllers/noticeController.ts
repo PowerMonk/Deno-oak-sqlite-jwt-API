2a43c063134bb1be2e4e9c4aaa195b74e3df9cf1
PowerMonk <qumk051105@itsuruapan.edu.mx>
2025-01-09 22:11:18 -0600

    Safety layer for bad requests in users' and notices' routes created

--- controllers/noticeController.ts
+++ controllers/noticeController.ts
@@ -6,112 +6,88 @@ import {
   deleteNotice,
   getNoticeByUserIdAndNoticeId,
   getNoticesByNoticeId,
+  getAllNotices,
 } from "../models/modelsMod.ts";
+import {
+  checkNoticeExistsByNoticeId,
+  checkNoticeExistsByUserId,
+} from "../utils/utilsMod.ts";
 
 export async function createNoticeHandler(ctx: RouterContext<string>) {
   const { title, content, userId } = await ctx.request.body.json();
-  try {
-    createNotice(title, content, userId);
-    ctx.response.status = 201;
-    ctx.response.body = {
-      message: `Notice created successfully! at ${new Date()}`,
-    };
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+
+  // These will throw if not found
+  checkNoticeExistsByUserId(userId);
+
+  // This guard clause is not needed since the  function will throw an error if the user is not found
+  // if (!user) return;
+
+  createNotice(title, content, userId);
+  ctx.response.status = 201;
+  ctx.response.body = {
+    message: `Notice created successfully! at ${new Date()}`,
+  };
 }
 
 export function getNoticesByUserHandler(ctx: RouterContext<string>) {
-  try {
-    const userId = +ctx.params.userId;
+  const userId = +ctx.params.userId;
 
-    if (isNaN(userId)) {
-      ctx.response.status = 400;
-      ctx.response.body = { error: "Invalid userId parameter" };
-      return;
-    }
-    const noticesObtainedById = getNoticesByUser(userId);
-    ctx.response.body = noticesObtainedById;
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+  checkNoticeExistsByUserId(userId);
+
+  const noticesObtainedById = getNoticesByUser(userId);
+  ctx.response.body = noticesObtainedById;
 }
+
 export function getNoticeByUserAndIdHandler(ctx: RouterContext<string>) {
-  try {
-    const userId = +ctx.params.userId;
-    const noticeId = +ctx.params.noticeId;
-    if (isNaN(userId) || isNaN(noticeId)) {
-      ctx.response.status = 400;
-      ctx.response.body = { error: "Invalid userId or noticeId parameter" };
-      return;
-    }
-    const noticeObtainedByUserAndById = getNoticeByUserIdAndNoticeId(
-      userId,
-      noticeId
-    );
-    if (!noticeObtainedByUserAndById) {
-      ctx.response.status = 404;
-      ctx.response.body = { error: "Notice not found" };
-      return;
-    }
-    ctx.response.body = noticeObtainedByUserAndById;
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+  const userId = +ctx.params.userId;
+  const noticeId = +ctx.params.noticeId;
+
+  checkNoticeExistsByUserId(userId);
+  checkNoticeExistsByNoticeId(noticeId);
+
+  const noticeObtainedByUserAndById = getNoticeByUserIdAndNoticeId(
+    userId,
+    noticeId
+  );
+
+  ctx.response.body = noticeObtainedByUserAndById;
 }
 
 export function getNoticesByNoticeIdHandler(ctx: RouterContext<string>) {
-  try {
-    const noticeId = +ctx.params.noticeId;
+  const noticeId = +ctx.params.noticeId;
 
-    if (isNaN(noticeId)) {
-      ctx.response.status = 400;
-      ctx.response.body = { error: "Invalid noticeId parameter" };
-      return;
-    }
-    const noticesObtainedByNoticeId = getNoticesByNoticeId(noticeId);
-    ctx.response.body = noticesObtainedByNoticeId;
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+  checkNoticeExistsByNoticeId(noticeId);
+
+  const noticesObtainedByNoticeId = getNoticesByNoticeId(noticeId);
+  ctx.response.body = noticesObtainedByNoticeId;
 }
 
 export async function noticeUpdaterHandler(ctx: RouterContext<string>) {
   const noticeId = +ctx.params.noticeId;
   //   const { title, content, noticeId } = await ctx.request.body.json();
   const { title, content } = await ctx.request.body.json();
-  try {
-    updateNotice(noticeId, title, content);
-    ctx.response.status = 200;
-    ctx.response.body = {
-      message: `Notice updated successfully! at ${new Date()}`,
-    };
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+
+  checkNoticeExistsByNoticeId(noticeId);
+
+  updateNotice(noticeId, title, content);
+  ctx.response.status = 200;
+  ctx.response.body = {
+    message: `Notice updated successfully! at ${new Date()}`,
+  };
 }
 
 export function noticeDeleterHandler(ctx: RouterContext<string>) {
   const noticeId = +ctx.params.noticeId;
-  try {
-    deleteNotice(noticeId);
-    ctx.response.status = 200;
-    ctx.response.body = {
-      message: `Notice deleted successfully! at ${new Date()}`,
-    };
-  } catch (error) {
-    console.log(error);
-    ctx.response.status = 400;
-    ctx.response.body = { error: error };
-  }
+
+  checkNoticeExistsByNoticeId(noticeId);
+
+  deleteNotice(noticeId);
+  ctx.response.status = 200;
+  ctx.response.body = {
+    message: `Notice deleted successfully! at ${new Date()}`,
+  };
+}
+export function getAllNoticesHandler(ctx: RouterContext<string>) {
+  const allNotices = getAllNotices();
+  ctx.response.body = allNotices;
 }
--- routes/noticeRoutes.ts
+++ routes/noticeRoutes.ts
@@ -6,12 +6,23 @@ import {
   noticeDeleterHandler,
   getNoticeByUserAndIdHandler,
   getNoticesByNoticeIdHandler,
+  getAllNoticesHandler,
 } from "../controllers/controllersMod.ts";
+import { authMiddleware } from "../auth/authMod.ts";
 
 router
   .post("/notices", validation.validateNoticeCreation, createNoticeHandler)
-  .get("/notices/:userId", getNoticesByUserHandler)
-  .get("/notices/anuncio/:noticeId", getNoticesByNoticeIdHandler)
-  .get("/notices/:userId/:noticeId", getNoticeByUserAndIdHandler)
-  .put("/notices/:noticeId", noticeUpdaterHandler)
-  .delete("/notices/:noticeId", noticeDeleterHandler);
+  .get("/notices", authMiddleware, getAllNoticesHandler)
+  .get("/notices/:userId", authMiddleware, getNoticesByUserHandler)
+  .get(
+    "/notices/anuncio/:noticeId",
+    authMiddleware,
+    getNoticesByNoticeIdHandler
+  )
+  .get(
+    "/notices/:userId/:noticeId",
+    authMiddleware,
+    getNoticeByUserAndIdHandler
+  )
+  .put("/notices/:noticeId", authMiddleware, noticeUpdaterHandler)
+  .delete("/notices/:noticeId", authMiddleware, noticeDeleterHandler);
